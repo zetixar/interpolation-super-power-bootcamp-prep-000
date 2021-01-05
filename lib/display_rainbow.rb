6565aa42b3b7bd6ad830f22d083f69d59427b941
Mohsen Danesh Pajouh <mohsendp@gmail.com>
2021-01-05 15:48:46 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,12 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
   puts colors[0]
-end
+  puts colors[1]
+  puts colors[2]
+  puts colors[3]
+  puts colors[4]
+  puts colors[5]
+  puts colors[6]
+  
+
 display_rainbow()
